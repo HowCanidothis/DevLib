3b906f4bdbd76f7ed744bfec5fbb9f3154c739fe
Andrey Zhuchkin <des_av@mail.ru>
2020-08-12 20:29:02 +0700

    Added temp tableView

---
+++ WidgetsModule/TableViews/widgetsadjustabletableview.cpp
@@ -0,0 +1,89 @@
+#include "widgetsadjustabletableview.h"
+
+#include <QHeaderView>
+#include <QScrollBar>
+#include <QTimer>
+#include <QTimerEvent>
+
+WidgetsAdjustableTableView::WidgetsAdjustableTableView(QWidget* parent)
+    : Super(parent)
+    , m_isDirty(true)
+    , m_contentsWidth(0)
+    , m_contentsHeight(0)
+{
+    setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    horizontalHeader()->setSectionResizeMode(QHeaderView::ResizeToContents);
+    verticalHeader()->setSectionResizeMode(QHeaderView::ResizeToContents);
+    verticalHeader()->setVisible(false);
+
+    addActions({ createAction("Dich", [this]{
+                     auto selIndexes = selectedIndexes();
+                     QSet<qint32> selectedRows;
+                     for(const auto& selInd : selIndexes) {
+                         selectedRows.insert(selInd.row());
+                     }
+                     if(!selectedRows.isEmpty()) {
+                        model()->removeRow(*selectedRows.begin());
+                     }
+                 }) });
+    setContextMenuPolicy(Qt::ActionsContextMenu);
+}
+
+QSize WidgetsAdjustableTableView::minimumSizeHint() const
+{
+    updateSizeHintCache();
+    return QSize(m_contentsWidth, m_contentsHeight);
+}
+
+QSize WidgetsAdjustableTableView::sizeHint() const
+{
+    updateSizeHintCache();
+    return QSize(m_contentsWidth, m_contentsHeight);
+}
+
+void WidgetsAdjustableTableView::doItemsLayout()
+{
+    Super::doItemsLayout();
+    m_isDirty = true;
+    updateGeometry();
+}
+
+void WidgetsAdjustableTableView::updateSizeHintCache() const
+{
+    if(m_isDirty) {
+        m_contentsWidth = 0;
+        m_contentsHeight = 0;
+
+        auto rowCount = model()->rowCount();
+        if(rowCount != 0) {
+            for(qint32 i(0); i < rowCount; i++) {
+                m_contentsHeight += rowHeight(i);
+            }
+            auto columnCount = model()->columnCount();
+            for(qint32 j(0); j < columnCount; j++) {
+                m_contentsWidth += columnWidth(j);
+            }
+
+            m_contentsWidth += contentsMargins().left() + contentsMargins().right();
+            m_contentsHeight += horizontalHeader()->sizeHint().height();
+        }
+        m_isDirty = false;
+    }
+}
+
+bool WidgetsAdjustableTableView::event(QEvent* event)
+{
+    if(event->type() == QEvent::Timer) {
+        m_isDirty = true;
+        updateGeometry();
+    }
+    return Super::event(event);
+}
+
+
+void WidgetsAdjustableTableView::setModel(QAbstractItemModel* model)
+{
+    Super::setModel(model);
+    updateGeometry();
+}
---
+++ WidgetsModule/TableViews/widgetsadjustabletableview.h
@@ -0,0 +1,31 @@
+#ifndef ADJUSTABLETABLEVIEW_H
+#define ADJUSTABLETABLEVIEW_H
+
+#include <QTableView>
+
+#include <SharedModule/External/external.hpp>
+
+class WidgetsAdjustableTableView : public QTableView
+{
+    using Super = QTableView;
+public:
+    WidgetsAdjustableTableView(QWidget* parent = nullptr);
+
+    // QWidget interface
+public:
+    QSize minimumSizeHint() const override;
+    QSize sizeHint() const override;
+    void doItemsLayout() override;
+    bool event(QEvent* event) override;
+    void setModel(QAbstractItemModel* model) override;
+
+private:
+    void updateSizeHintCache() const;
+
+private:
+    mutable bool m_isDirty;
+    mutable qint32 m_contentsWidth;
+    mutable qint32 m_contentsHeight;
+};
+
+#endif // ADJUSTABLETABLEVIEW_H
