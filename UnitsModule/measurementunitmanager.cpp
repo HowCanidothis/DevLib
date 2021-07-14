ace54736152dc9d9529f2ed4ddd52ca1bac6e6ac
smag <smagilov@mail.ru>
2021-07-14 19:17:46 +0700

    merge fix

--- UnitsModule/measurementunitmanager.cpp
+++ UnitsModule/measurementunitmanager.cpp
@@ -2,11 +2,17 @@
 
 #include <qmath.h>
 
-MeasurementUnit::MeasurementUnit(const FTranslationHandler& translationHandler, double multiplierUnitToBase)
-    : Label(translationHandler)
-    , m_multiplier(multiplierUnitToBase)
-{
+static const Name UNIT_SYSTEM_API         = "API";
+static const Name UNIT_SYSTEM_API_USFT    = "API USFT";
+static const Name UNIT_SYSTEM_SI          = "SI";
 
+MeasurementUnit::MeasurementUnit(const Name& id, const FTranslationHandler& fullLabelTrHandler, const FTranslationHandler& translationHandler, double multiplierUnitToBase)
+    : Id(id)
+      , LabelFull(fullLabelTrHandler)
+      , Label(translationHandler)
+      , m_multiplier(multiplierUnitToBase)
+{
+    
 }
 
 double MeasurementUnit::FromUnitToBase(double unitValue) const
@@ -19,32 +25,37 @@ double MeasurementUnit::FromBaseToUnit(double baseValue) const
     return baseValue / m_multiplier;
 }
 
-Measurement::Measurement()
-    : m_currentUnit(nullptr)
+Measurement::Measurement(const QString& label)
+    : Label(label)
+      , Precision(2)
+      , m_wrapper(::make_shared<WPSCUnitTableWrapper>())
+      , m_currentUnit(nullptr)
 {
-
+    CurrentUnitId.Subscribe([this]{
+        m_currentConnections.clear();
+        Q_ASSERT(m_metricUnits.contains(CurrentUnitId));
+        m_currentUnit = m_metricUnits[CurrentUnitId];
+        CurrentUnitLabel.ConnectFrom(m_currentUnit->Label).MakeSafe(m_currentConnections);
+    });
+    
+    OnChanged.Subscribe({&Label.OnChange,
+                           &CurrentUnitId.OnChange,
+                           &CurrentUnitLabel.OnChange,
+                           &Precision.OnChange});
 }
 
-Measurement& Measurement::AddUnit(const Name& id, const MeasurementUnit* unit)
+Measurement& Measurement::AddUnit(const MeasurementUnit* unit)
 {
-    Q_ASSERT(!m_metricUnits.contains(id));
-
+    Q_ASSERT(!m_metricUnits.contains(unit->Id));
+    
     if(m_currentUnit == nullptr) {
         m_currentUnit = unit;
-        CurrentLabel.ConnectFrom(unit->Label).MakeSafe(m_currentConnections);
+        CurrentUnitLabel.ConnectFrom(unit->Label).MakeSafe(m_currentConnections);
     }
-
-    m_metricUnits.insert(id, unit);
-
+    
+    m_metricUnits.insert(unit->Id, unit);
+    m_wrapper->Append(unit);
     return *this;
-}
-
-void Measurement::SetCurrent(const Name& id)
-{
-    Q_ASSERT(m_metricUnits.contains(id));
-    m_currentUnit = m_metricUnits[id];
-    CurrentLabel.ConnectFrom(m_currentUnit->Label).MakeSafe(m_currentConnections);
-    OnCurrentChanged();
 }
 
 double Measurement::CurrentUnitToBaseValue(double currentUnit) const
@@ -57,11 +68,6 @@ double Measurement::BaseValueToCurrentUnit(double baseValue) const
     return m_currentUnit->FromBaseToUnit(baseValue);
 }
 
-const MeasurementUnit* Measurement::getCurrentMeasurementUnit() const
-{
-    return m_currentUnit;
-}
-
 const MeasurementUnit* Measurement::FindUnit(const Name &metric) const
 {
     auto foundIt = m_metricUnits.find(metric);
@@ -72,28 +78,76 @@ const MeasurementUnit* Measurement::FindUnit(const Name &metric) const
 }
 
 MeasurementManager::MeasurementManager()
+    : m_systemWrapper(::make_shared<WPSCUnitSystemTableWrapper>())
+      , m_measurmentWrapper(::make_shared<WPSCUnitMeasurementTableWrapper>())
 {
     AddMeasurement(MEASUREMENT_ANGLES)
-            .AddUnit("Degree", &AngleUnits::Degrees)
-            .AddUnit("Radian", &AngleUnits::Radians);
-
+        .AddUnit(&AngleUnits::Degrees)
+        .AddUnit(&AngleUnits::Radians);
+    
     AddMeasurement(MEASUREMENT_DISTANCES)
-            .AddUnit("Usfeets", &DistanceUnits::USFeets)
-            .AddUnit("Feets", &DistanceUnits::Feets)
-            .AddUnit("Meters", &DistanceUnits::Meters);
-
+        .AddUnit(&DistanceUnits::USFeets)
+        .AddUnit(&DistanceUnits::Feets)
+        .AddUnit(&DistanceUnits::Meters);
+    
     AddMeasurement(MEASUREMENT_FIELD_STRENGTH)
-            .AddUnit("NanoTeslas", &FieldStrengthUnits::NanoTeslas);
-
+        .AddUnit(&FieldStrengthUnits::NanoTeslas);
+    
     AddMeasurement(MEASUREMENT_DLS)
-            .AddUnit("DegreeUSFeet", &DLSUnits::DegreeUSFeet)
-            .AddUnit("DegreeFeet", &DLSUnits::DegreeFeet)
-            .AddUnit("DegreeMeter", &DLSUnits::DegreeMeter);
+        .AddUnit(&DLSUnits::DegreeUSFeet)
+        .AddUnit(&DLSUnits::DegreeFeet)
+        .AddUnit(&DLSUnits::DegreeMeter)
+        .AddUnit(&DLSUnits::RadMeter);
+    
+    AddSystem(UNIT_SYSTEM_API)
+        .AddParameter(MEASUREMENT_ANGLES,            {AngleUnits::Degrees.Id,            2})
+        .AddParameter(MEASUREMENT_DISTANCES,         {DistanceUnits::Feets.Id,           2})
+        .AddParameter(MEASUREMENT_FIELD_STRENGTH,    {FieldStrengthUnits::NanoTeslas.Id, 2})
+        .AddParameter(MEASUREMENT_DLS,               {DLSUnits::DegreeFeet.Id,           2});
+    
+    AddSystem(UNIT_SYSTEM_API_USFT)
+        .AddParameter(MEASUREMENT_ANGLES,            {AngleUnits::Degrees.Id,            2})
+        .AddParameter(MEASUREMENT_DISTANCES,         {DistanceUnits::USFeets.Id,         2})
+        .AddParameter(MEASUREMENT_FIELD_STRENGTH,    {FieldStrengthUnits::NanoTeslas.Id, 2})
+        .AddParameter(MEASUREMENT_DLS,               {DLSUnits::DegreeUSFeet.Id,         2});
+    
+    AddSystem(UNIT_SYSTEM_SI)
+        .AddParameter(MEASUREMENT_ANGLES,            {AngleUnits::Radians.Id,           2})
+        .AddParameter(MEASUREMENT_DISTANCES,         {DistanceUnits::Meters.Id,         2})
+        .AddParameter(MEASUREMENT_FIELD_STRENGTH,    {FieldStrengthUnits::NanoTeslas.Id,2})
+        .AddParameter(MEASUREMENT_DLS,               {DLSUnits::RadMeter.Id,            2});
+    
+    CurrentMeasurementSystem.SetAndSubscribe([this]{
+        const auto& system = GetSystem(CurrentMeasurementSystem);
+        Q_ASSERT(system != nullptr);
+        
+        m_connections.clear();
+        for(auto iter = system->begin(); iter != system->end(); ++iter){
+            const auto& measurement = GetMeasurement(iter.key());
+            auto* parameters = &iter.value();
+            Q_ASSERT(measurement != nullptr);
+            measurement->CurrentUnitId = parameters->UnitId;
+            measurement->Precision = parameters->UnitPrecision;
+            
+            measurement->CurrentUnitId.OnChange.Connect(this, [measurement, parameters]{parameters->UnitId = measurement->CurrentUnitId; }).MakeSafe(m_connections);
+            measurement->Precision.OnChange.Connect(this, [parameters, measurement]{parameters->UnitPrecision = measurement->Precision; }).MakeSafe(m_connections);
+        }
+    });
 }
 
 const MeasurementPtr& MeasurementManager::GetMeasurement(const Name& name) const
 {
     static MeasurementPtr defaultResult;
+    auto foundIt = m_metricMeasurements.find(name);
+    if(foundIt != m_metricMeasurements.end()) {
+        return foundIt.value();
+    }
+    return defaultResult;
+}
+
+const MeasurementSystemPtr& MeasurementManager::GetSystem(const Name & name) const
+{
+    static MeasurementSystemPtr defaultResult;
     auto foundIt = m_metricSystems.find(name);
     if(foundIt != m_metricSystems.end()) {
         return foundIt.value();
@@ -103,9 +157,26 @@ const MeasurementPtr& MeasurementManager::GetMeasurement(const Name& name) const
 
 Measurement& MeasurementManager::AddMeasurement(const Name &name)
 {
+    Q_ASSERT(!m_metricMeasurements.contains(name));
+    auto result = ::make_shared<Measurement>(name.AsString());
+    m_metricMeasurements.insert(name, result);
+    int index = m_measurmentWrapper->GetSize();
+    m_measurmentWrapper->Append(result);
+    result->OnChanged.Connect(this, [this, index]{m_measurmentWrapper->Edit(index, [](const MeasurementPtr& p){ qDebug() << "Change " << p->Label; }); });
+    return *result;
+}
+
+MeasurementSystem & MeasurementManager::AddSystem(const Name& name)
+{
     Q_ASSERT(!m_metricSystems.contains(name));
-    auto result = ::make_shared<Measurement>();
+    auto result = ::make_shared<MeasurementSystem>(name);
     m_metricSystems.insert(name, result);
+    m_systemWrapper->Append(result);
+    
+    if(CurrentMeasurementSystem.Native().IsNull()) {
+        CurrentMeasurementSystem = name;
+    }
+    
     return *result;
 }
 
@@ -117,8 +188,8 @@ MeasurementManager& MeasurementManager::GetInstance()
 
 const MeasurementUnit* MeasurementManager::GetCurrentUnit(const Name& systemName) const
 {
-    Q_ASSERT(m_metricSystems.contains(systemName));
-    return m_metricSystems[systemName]->getCurrentMeasurementUnit();
+    Q_ASSERT(m_metricMeasurements.contains(systemName));
+    return m_metricMeasurements[systemName]->GetCurrentUnit();
 }
 
 void MeasurementProperty::Connect(LocalPropertyDouble* baseValueProperty)
@@ -126,17 +197,17 @@ void MeasurementProperty::Connect(LocalPropertyDouble* baseValueProperty)
     m_connections.clear();
     if(baseValueProperty != nullptr) {
         baseValueProperty->ConnectBoth(Value, [this](double baseValue){
-            return m_metricSystem->BaseValueToCurrentUnit(baseValue);
-        }, [this](double currentUnit){
-            return m_metricSystem->CurrentUnitToBaseValue(currentUnit);
-        }).MakeSafe(m_connections);
-
+                                 return m_metricSystem->BaseValueToCurrentUnit(baseValue);
+                             }, [this](double currentUnit){
+                                 return m_metricSystem->CurrentUnitToBaseValue(currentUnit);
+                             }).MakeSafe(m_connections);
+        
         auto updateMinMax = [baseValueProperty, this]{
             Value.SetMinMax(m_metricSystem->BaseValueToCurrentUnit(baseValueProperty->GetMin()), m_metricSystem->BaseValueToCurrentUnit(baseValueProperty->GetMax()));
         };
         baseValueProperty->OnMinMaxChanged.Connect(this, updateMinMax).MakeSafe(m_connections);
         updateMinMax();
-
+        
     }
     m_currentValue = baseValueProperty;
 }
@@ -145,9 +216,9 @@ MeasurementProperty::MeasurementProperty(const Name& systemName)
     : m_currentValue(nullptr)
 {
     m_metricSystem = MeasurementManager::GetInstance().GetMeasurement(systemName);
-    m_systemConnection = m_metricSystem->OnCurrentChanged.Connect(this, [this]{
-        Connect(m_currentValue);
-    }).MakeSafe();
+    m_systemConnection = m_metricSystem->OnChanged.Connect(this, [this]{
+                                                      Connect(m_currentValue);
+                                                  }).MakeSafe();
 }
 
 void MeasurementTranslatedString::AttachToTranslatedString(TranslatedString& string, const TranslatedString::FTranslationHandler& translationHandler, const QVector<Name>& metrics)
@@ -157,7 +228,7 @@ void MeasurementTranslatedString::AttachToTranslatedString(TranslatedString& str
     for(const auto& metric : metrics) {
         auto foundIt = uniqueMetrics.find(metric);
         if(foundIt == uniqueMetrics.end()) {
-            string.Retranslate.ConnectFrom(MeasurementManager::GetInstance().GetMeasurement(metric)->CurrentLabel.OnChange).MakeSafe(connections);
+            string.Retranslate.ConnectFrom(MeasurementManager::GetInstance().GetMeasurement(metric)->CurrentUnitLabel.OnChange).MakeSafe(connections);
             uniqueMetrics.insert(metric);
         }
     }
@@ -176,7 +247,7 @@ TranslatedString::FTranslationHandler MeasurementTranslatedString::generateTrans
         while((index = regExp.indexIn(string, index)) != -1 && it != metrics.end()) {
             resultString.append(QStringView(string.begin() + stringIndex, string.begin() + index).toString());
             auto metricSystem = MeasurementManager::GetInstance().GetMeasurement(*it);
-            resultString.append(metricSystem->CurrentLabel);
+            resultString.append(metricSystem->CurrentUnitLabel);
             it++;
             index += regExp.matchedLength();
             stringIndex = index;
@@ -185,4 +256,22 @@ TranslatedString::FTranslationHandler MeasurementTranslatedString::generateTrans
 
         return resultString;
     };
+}
+
+MeasurementSystem & MeasurementSystem::AddParameter(const Name & measurmentType, const MeasurementParams & param){
+    Q_ASSERT(!contains(measurmentType));
+    insert(measurmentType, param);
+    return *this;
+}
+
+const MeasurementParams & MeasurementSystem::GetParameter(const Name & measurmentType) const
+{
+    auto iter = find(measurmentType);
+    Q_ASSERT(iter != end());
+    return iter.value();
+}
+
+MeasurementParams::MeasurementParams(const Name & measurmentType){
+    const auto& system = MeasurementManager::GetInstance().GetSystem(UNIT_SYSTEM_API);
+    *this = system->GetParameter(measurmentType);
 }
--- UnitsModule/measurementunitmanager.h
+++ UnitsModule/measurementunitmanager.h
@@ -2,125 +2,191 @@
 #define MEASUREMENTUNITMANAGER_H
 
 #include <PropertiesModule/internal.hpp>
-
+#include <WidgetsModule/internal.hpp>
 #include <qmath.h>
 
 static constexpr double METERS_TO_FEETS_MULTIPLIER = 3.280839895;
 static constexpr double USFEETS_TO_FEETS_MULTIPLIER = 1.000002;
 static constexpr double DEGREES_TO_RADIANS = M_PI / 180.0;
 
+static const Name MEASUREMENT_ANGLES         = "ANGLES";
+static const Name MEASUREMENT_DISTANCES      = "DISTANCES";
+static const Name MEASUREMENT_FIELD_STRENGTH = "FIELD_STRENGTH";
+static const Name MEASUREMENT_DLS            = "DLS";
+
 class MeasurementUnit
 {
 public:
     using FTransform = std::function<double (double)>;
     using FTranslationHandler = std::function<QString ()>;
-    MeasurementUnit(const FTranslationHandler& translationHandler, double multiplierUnitToBase);
-
+    MeasurementUnit(const Name& id, const FTranslationHandler& fullLabelTrHandler, const FTranslationHandler& translationHandler, double multiplierUnitToBase);
+    
     double FromUnitToBase(double unitValue) const;
     double FromBaseToUnit(double baseValue) const;
-
+    
     FTransform GetUnitToBaseConverter() const { return [this](double unit) { return FromUnitToBase(unit); }; }
     FTransform GetBaseToUnitConverter() const { return [this](double base) { return FromBaseToUnit(base); }; }
-
+    
+    const Name Id;
+    mutable TranslatedString LabelFull;
     mutable TranslatedString Label;
+    
 private:
     double m_multiplier;
 };
+Q_DECLARE_METATYPE(const MeasurementUnit*)
+using WPSCUnitTableWrapper = TModelsTableWrapper<QVector<const MeasurementUnit*>>;
+using WPSCUnitTableWrapperPtr = SharedPointer<WPSCUnitTableWrapper>;
 
 namespace DistanceUnits
 {
-    static const MeasurementUnit Meters([]{ return QObject::tr("m"); }, 3.280839895);
-    static const MeasurementUnit Feets([]{ return QObject::tr("ft"); }, 1.0);
-    static const MeasurementUnit USFeets([]{ return QObject::tr("usft"); }, USFEETS_TO_FEETS_MULTIPLIER);
+    static const MeasurementUnit Meters ("Meters", []{return QObject::tr("meters");}, []{ return QObject::tr("m"); }, 3.280839895);
+    static const MeasurementUnit Feets  ("Feets"  , []{return QObject::tr("feets");},  []{ return QObject::tr("ft"); }, 1.0);
+    static const MeasurementUnit USFeets("Usfeets", []{return QObject::tr("usfeets");},[]{ return QObject::tr("usft"); }, USFEETS_TO_FEETS_MULTIPLIER);
 };
 
 namespace AngleUnits
 {
-    static const MeasurementUnit Radians([]{ return QObject::tr("rad"); }, 1.0);
-    static const MeasurementUnit Degrees([]{ return "В°"; }, DEGREES_TO_RADIANS);
+    static const MeasurementUnit Radians("Radian", []{return QObject::tr("radians");}, []{ return QObject::tr("rad"); }, 1.0);
+    static const MeasurementUnit Degrees("Degree", []{return QObject::tr("degrees");}, []{ return "°"; }, DEGREES_TO_RADIANS);
 };
 
 namespace FieldStrengthUnits
 {
-    static const MeasurementUnit NanoTeslas([]{ return QObject::tr("nT"); }, 1.0);
+    static const MeasurementUnit NanoTeslas("NanoTeslas", []{return QObject::tr("nano tesla");}, []{ return QObject::tr("nT"); }, 1.0);
 };
 
 namespace DLSUnits
 {
-    static const MeasurementUnit RadFeet([]{ return QObject::tr("rad/100ft"); }, 1.0);
-    static const MeasurementUnit DegreeFeet([]{ return QObject::tr("В°/100ft"); }, DEGREES_TO_RADIANS);
-    static const MeasurementUnit DegreeUSFeet([]{ return QObject::tr("В°/100usft"); }, DEGREES_TO_RADIANS / USFEETS_TO_FEETS_MULTIPLIER);
-    static const MeasurementUnit DegreeMeter([]{ return QObject::tr("В°/30m"); }, DEGREES_TO_RADIANS * (100.0 / METERS_TO_FEETS_MULTIPLIER) / 30.0);
+    static const MeasurementUnit RadFeet     ("RadianFeet"  , []{return QObject::tr("radians / 100 feets");}, []{ return QObject::tr("rad/100ft"); }, 1.0);
+    static const MeasurementUnit RadMeter    ("RadianMeter" , []{return QObject::tr("radians / 30 meters");}, []{ return QObject::tr("rad/30m"); }, DEGREES_TO_RADIANS / (100.0 / 30.0));
+    static const MeasurementUnit DegreeFeet  ("DegreeFeet"  , []{return QObject::tr("degrees / 100 feets");}, []{ return QObject::tr("В°/100ft"); }, DEGREES_TO_RADIANS);
+    static const MeasurementUnit DegreeUSFeet("DegreeUSFeet", []{return QObject::tr("degrees / 100 usfeets");}, []{ return QObject::tr("В°/100usft"); }, DEGREES_TO_RADIANS / USFEETS_TO_FEETS_MULTIPLIER);
+    static const MeasurementUnit DegreeMeter ("DegreeMeter" , []{return QObject::tr("degrees / 30 meters");}, []{ return QObject::tr("В°/30m"); }, DEGREES_TO_RADIANS * (100.0 / METERS_TO_FEETS_MULTIPLIER) / 30.0);
 }
 
 class Measurement
 {
 public:
-    Measurement();
-
-    void SetCurrent(const Name& id);
-    Measurement& AddUnit(const Name& id, const MeasurementUnit* unit);
+    Measurement(const QString& label);
+    
+    Measurement& AddUnit(const MeasurementUnit* unit);
     const MeasurementUnit* FindUnit(const Name& metric) const;
-
+    
     double CurrentUnitToBaseValue(double currentUnit) const;
     double BaseValueToCurrentUnit(double baseValue) const;
-
-    LocalPropertyString CurrentLabel;
-    LocalPropertyInt CurrentPrecision;
-
-    Dispatcher OnCurrentChanged;
-
+    
+    const MeasurementUnit* GetCurrentUnit() const { return m_currentUnit; }
+    const WPSCUnitTableWrapperPtr& GetTableWrapper() const { return m_wrapper; }
+    
+    LocalPropertyString Label;
+    LocalPropertyInt Precision;
+    
+    LocalProperty<Name> CurrentUnitId;
+    LocalPropertyString CurrentUnitLabel;
+    
+    DispatchersCommutator OnChanged;
+    
 private:
     friend class MeasurementManager;
-    const MeasurementUnit* getCurrentMeasurementUnit() const;
-
+    
+    WPSCUnitTableWrapperPtr m_wrapper;
     QHash<Name, const MeasurementUnit*> m_metricUnits;
+    
     const MeasurementUnit* m_currentUnit;
     DispatcherConnectionsSafe m_currentConnections;
 };
-
 using MeasurementPtr = SharedPointer<Measurement>;
+Q_DECLARE_METATYPE(SharedPointer<Measurement>);
+using WPSCUnitMeasurementTableWrapper = TModelsTableWrapper<QVector<MeasurementPtr>>;
+using WPSCUnitMeasurementTableWrapperPtr = SharedPointer<WPSCUnitMeasurementTableWrapper>;
 
-static const Name MEASUREMENT_ANGLES = "ANGLES";
-static const Name MEASUREMENT_DISTANCES = "DISTANCES";
-static const Name MEASUREMENT_FIELD_STRENGTH = "FIELD_STRENGTH";
-static const Name MEASUREMENT_DLS = "DLS";
 
 struct MeasurementParams
 {
-    Name Measurement;
-    qint32 precision;
+    MeasurementParams(){}
+    MeasurementParams(const Name& id, int precision): UnitId(id), UnitPrecision(precision){}
+    MeasurementParams(const Name& measurmentType);
+    Name UnitId;
+    qint32 UnitPrecision;
+    
+    template<typename Buffer>
+    void Serialize (Buffer& buffer) {
+        buffer << UnitId;
+        buffer << UnitPrecision;
+    }
+};
+class MeasurementSystem : public QHash<Name,MeasurementParams>
+{
+    using Super = QHash<Name,MeasurementParams>;
+public:
+    MeasurementSystem(){}
+    MeasurementSystem(const Name& label) : Label(label.AsString()) {}
+    
+    LocalPropertyString Label;
+    MeasurementSystem& AddParameter(const Name& measurmentType, const MeasurementParams& param);
+    const MeasurementParams& GetParameter(const Name& measurmentType) const;
+    
+public:
+    template<typename Buffer>
+    void Serialize (Buffer& buffer) {
+        buffer << Label;
+        buffer << *reinterpret_cast<Super*>(this);
+    }
 };
 
-using MeasurementSystem = QHash<Name,MeasurementParams>;
+using MeasurementSystemPtr = SharedPointer<MeasurementSystem>;
+using WPSCUnitSystemTableWrapper = TModelsTableWrapper<QVector<MeasurementSystemPtr>>;
+using WPSCUnitSystemTableWrapperPtr = SharedPointer<WPSCUnitSystemTableWrapper>;
+Q_DECLARE_METATYPE(SharedPointer<MeasurementSystem>);
 
 class MeasurementManager
 {
     Q_DECLARE_TR_FUNCTIONS(MeasurementManager)
     MeasurementManager();
 public:
-
-    Measurement& AddMeasurement(const Name& name);
-    const MeasurementPtr& GetMeasurement(const Name& name) const;
     static MeasurementManager& GetInstance();
-
+    
+    Measurement& AddMeasurement(const Name& name);
+    MeasurementSystem& AddSystem(const Name& name);
+    const MeasurementPtr& GetMeasurement(const Name& name) const;
+    const MeasurementSystemPtr& GetSystem(const Name& name) const;
+    
     const MeasurementUnit* GetCurrentUnit(const Name& systemName) const;
-
+    
     static constexpr double MetersToFeets(double meters) { return meters * METERS_TO_FEETS_MULTIPLIER; }
     static constexpr double FeetsToMeters(double feets) { return feets / METERS_TO_FEETS_MULTIPLIER; }
-
+    
+    LocalProperty<Name> CurrentMeasurementSystem;
+    
+    
+    const WPSCUnitSystemTableWrapperPtr& GetSystemsWrapper() const { return m_systemWrapper; }
+    const WPSCUnitMeasurementTableWrapperPtr& GetMeasurementWrapper() const { return m_measurmentWrapper; }
+    
 private:
-    QHash<Name, MeasurementPtr> m_metricSystems;
+    DispatcherConnectionsSafe m_connections;
+    WPSCUnitSystemTableWrapperPtr m_systemWrapper;
+    WPSCUnitMeasurementTableWrapperPtr m_measurmentWrapper;
+    
+    QHash<Name, MeasurementSystemPtr> m_metricSystems;
+    QHash<Name, MeasurementPtr> m_metricMeasurements;
+    
+public:
+    template<typename Buffer>
+    void Serialize (Buffer& buffer) {
+        buffer << m_metricSystems;
+        buffer << CurrentMeasurementSystem;
+    }
 };
 
 class MeasurementProperty
 {
 public:
     MeasurementProperty(const Name& systemName);
-
+    
     void Connect(LocalPropertyDouble* baseValueProperty);
     LocalPropertyDouble Value;
-
+    
 private:
     LocalPropertyDouble* m_currentValue;
     DispatcherConnectionsSafe m_connections;
