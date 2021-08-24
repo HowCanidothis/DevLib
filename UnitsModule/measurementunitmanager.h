bd5b7ad261d97db3205f4ef5768bb5224e388b02
Andrey Zhuchkin <des_av@mail.ru>
2021-08-24 18:07:43 +0700

    added static unit conversion

--- UnitsModule/measurementunitmanager.h
+++ UnitsModule/measurementunitmanager.h
@@ -150,6 +150,9 @@ public:
     
     const MeasurementUnit* GetCurrentUnit(const Name& systemName) const;
     
+    static constexpr double UsFeetsToFeets(double meters) { return meters * USFEETS_TO_FEETS_MULTIPLIER; }
+    static constexpr double FeetsToUsFeets(double feets) { return feets / USFEETS_TO_FEETS_MULTIPLIER; }
+
     static constexpr double MetersToFeets(double meters) { return meters * METERS_TO_FEETS_MULTIPLIER; }
     static constexpr double FeetsToMeters(double feets) { return feets / METERS_TO_FEETS_MULTIPLIER; }
     
