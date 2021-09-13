94a495c7ee40e86c10975b1a0c5be710ab7d3d34
Andrey Zhuchkin <des_av@mail.ru>
2021-09-13 18:58:53 +0700

    Bug fixes

--- WidgetsModule/Utils/styleutils.h
+++ WidgetsModule/Utils/styleutils.h
@@ -8,6 +8,7 @@ class StyleUtils
     StyleUtils();
 public:
 
+    static void UpdateStyle(class QWidget* target, bool recursive = false);
     static void ApplyStyleProperty(const char* propertyName, class QWidget* target, const QVariant& value, bool recursive = false);
     // Qt bug? Widgets which don't participate at layout system does not update their padding
     // SizeAdjuster fixes this issue
