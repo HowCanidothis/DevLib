714655e02b8bac39579ad8975d6e129e2f07610e
smag <smagilov@mail.ru>
2021-11-01 18:27:53 +0700

    area unit

--- UnitsModule/MeasurementTypes/areadeclarations.h
+++ UnitsModule/MeasurementTypes/areadeclarations.h
@@ -9,6 +9,7 @@ static constexpr double SQ_METERS_TO_SQ_FEETS_MULTIPLIER = METERS_TO_FEETS_MULTI
 namespace AreaUnits
 {
     static const MeasurementUnit SqMeters      ("SqMeters", []{return QObject::tr("square meters");}, []{ return QObject::tr("m2"); }, SQ_METERS_TO_SQ_FEETS_MULTIPLIER);
+    static const MeasurementUnit SqCentimeters ("SqCentimeters", []{return QObject::tr("square centimeters");}, []{ return QObject::tr("cm2"); }, SQ_METERS_TO_SQ_FEETS_MULTIPLIER/10e4);
     static const MeasurementUnit SqMilimeters  ("SqMilimeters", []{return QObject::tr("square milimeters");}, []{ return QObject::tr("mm2"); }, SQ_METERS_TO_SQ_FEETS_MULTIPLIER/10e6);
     static const MeasurementUnit SqFeets       ("SqFeets", []{return QObject::tr("square feet");}, []{ return QObject::tr("ft2"); }, 1);
     static const MeasurementUnit SqInches      ("SqInches", []{return QObject::tr("square inches");}, []{ return QObject::tr("in2"); }, [](double v){ return v/144.0; }, [](double v){ return v*144.0;});
