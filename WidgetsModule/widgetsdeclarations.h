6d233dd79b6c454aa5125afe25fe78b9055b1b79
Andrey Zhuchkin <des_av@mail.ru>
2022-03-17 16:55:42 +0700

    build fix

--- WidgetsModule/widgetsdeclarations.h
+++ WidgetsModule/widgetsdeclarations.h
@@ -12,8 +12,8 @@ struct DescColumnsParams
 
         ColumnParam& SetVisible(bool visible) { Visible = visible; return *this; }
         ColumnParam& SetCanBeHidden(bool canBeHidden) { CanBeHidden = canBeHidden; return *this; }
-        ColumnParam& HideColumn() { Visible = false; CanBeHidden = true; }
-        ColumnParam& RemoveColumn() { Visible = false; CanBeHidden = false; }
+        ColumnParam& HideColumn() { Visible = false; CanBeHidden = true; return *this;  }
+        ColumnParam& RemoveColumn() { Visible = false; CanBeHidden = false; return *this;  }
     };
 
     DescColumnsParams(const QSet<qint32>& ignoreColumns);
