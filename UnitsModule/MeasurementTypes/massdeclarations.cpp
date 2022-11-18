62714fc97225535048a64bdedba00dd27c479c89
smag <smagilov@mail.ru>
2022-11-18 08:47:15 +0700

    fix unit

--- UnitsModule/MeasurementTypes/massdeclarations.cpp
+++ UnitsModule/MeasurementTypes/massdeclarations.cpp
@@ -8,7 +8,7 @@ namespace MassUnits
     IMPLEMENT_MEASUREMENT_UNIT(Grams, []{return QObject::tr("grams");     }, []{ return QObject::tr("g");   }, 0.001  );
     IMPLEMENT_MEASUREMENT_UNIT(Tonnes, []{return QObject::tr("tonnes");    }, []{ return QObject::tr("t");   }, 1000.0 );
     IMPLEMENT_MEASUREMENT_UNIT(Pounds, []{return QObject::tr("pounds");    }, []{ return QObject::tr("lbs"); }, 0.453592);
-    IMPLEMENT_MEASUREMENT_UNIT(Kilopounds, []{return QObject::tr("kilopounds");}, []{ return QObject::tr("klbf");}, 453.592);
+    IMPLEMENT_MEASUREMENT_UNIT(Kilopounds, []{return QObject::tr("kilopounds");}, []{ return QObject::tr("klbs");}, 453.592);
 }
 
 IMPLEMENT_MEASUREMENT(Mass, TR(MeasurementTr::tr("Mass")))
