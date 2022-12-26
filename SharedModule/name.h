6878ffbd56d924023a029cdc43f99a90f557bc47
AndreyZh <des_av@mail.ru>
2022-12-26 18:05:22 +0600

    != operator

--- SharedModule/name.h
+++ SharedModule/name.h
@@ -33,6 +33,7 @@ public:
 
     Name& operator=(const Name& another);
     bool operator==(const Name& another) const { return another.m_value == m_value && (m_text.isEmpty() || another.m_text.isEmpty() || m_text == another.m_text); }
+    bool operator!=(const Name& another) const { return !operator==(another); }
 
     static Name FromValue(qint64 value) { Name result; result.m_value = value; return result; }
 
