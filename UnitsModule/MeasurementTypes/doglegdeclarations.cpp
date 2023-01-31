07a8f897bca6c1998ac658b73df305a32df2c981
smag <smagilov@mail.ru>
2023-01-31 12:16:40 +0700

    rename

--- UnitsModule/MeasurementTypes/doglegdeclarations.cpp
+++ UnitsModule/MeasurementTypes/doglegdeclarations.cpp
@@ -4,12 +4,12 @@
 
 namespace DLSUnits
 {
-    IMPLEMENT_MEASUREMENT_UNIT(RadFeet, []{return QObject::tr("rad per 100ft");}, []{ return QObject::tr("rad/100ft"); }, METERS_TO_FEETS_MULTIPLIER / 100.0);
+    IMPLEMENT_MEASUREMENT_UNIT(Rad100Feet, []{return QObject::tr("rad per 100ft");}, []{ return QObject::tr("rad/100ft"); }, METERS_TO_FEETS_MULTIPLIER / 100.0);
     IMPLEMENT_MEASUREMENT_UNIT(RadMeter, []{return QObject::tr("rad per m");}, []{ return QObject::tr("rad/m"); }, 1.0);
     IMPLEMENT_MEASUREMENT_UNIT(Rad30Meter, []{return QObject::tr("rad per 30m");}, []{ return QObject::tr("rad/30m"); }, 1.0/30);
-    IMPLEMENT_MEASUREMENT_UNIT(DegreeFeet, []{return QObject::tr("deg per 100ft");}, []{ return QObject::tr("°/100ft"); }, DEGREES_TO_RADIANS * METERS_TO_FEETS_MULTIPLIER / 100.0);
-    IMPLEMENT_MEASUREMENT_UNIT(DegreeUSFeet, []{return QObject::tr("deg per 100usft");}, []{ return QObject::tr("°/100usft"); }, DEGREES_TO_RADIANS * METERS_TO_FEETS_MULTIPLIER / (100.0 * USFEETS_TO_FEETS_MULTIPLIER));
-    IMPLEMENT_MEASUREMENT_UNIT(DegreeMeter, []{return QObject::tr("deg per 30m");}, []{ return QObject::tr("°/30m"); }, DEGREES_TO_RADIANS/30);
+    IMPLEMENT_MEASUREMENT_UNIT(Degree100Feet, []{return QObject::tr("deg per 100ft");}, []{ return QObject::tr("°/100ft"); }, DEGREES_TO_RADIANS * METERS_TO_FEETS_MULTIPLIER / 100.0);
+    IMPLEMENT_MEASUREMENT_UNIT(Degree100USFeet, []{return QObject::tr("deg per 100usft");}, []{ return QObject::tr("°/100usft"); }, DEGREES_TO_RADIANS * METERS_TO_FEETS_MULTIPLIER / (100.0 * USFEETS_TO_FEETS_MULTIPLIER));
+    IMPLEMENT_MEASUREMENT_UNIT(Degree30Meter, []{return QObject::tr("deg per 30m");}, []{ return QObject::tr("°/30m"); }, DEGREES_TO_RADIANS/30);
 }
 
 IMPLEMENT_MEASUREMENT(DLS, TR(MeasurementTr::tr("Dogleg Severity")))
