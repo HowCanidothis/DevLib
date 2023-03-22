c52a0465240113e4daf35f859375660d84f49006
Smag <smagilov@mail.ru>
2023-03-22 19:23:16 +0700

    bug fix

--- WidgetsModule/Utils/widgethelpers.cpp
+++ WidgetsModule/Utils/widgethelpers.cpp
@@ -327,11 +327,14 @@ QHeaderView* WidgetTableViewWrapper::InitializeVertical(const DescTableViewParam
 
     if(params.UseMeasurementDelegates){
         auto* model = tableView->model();
-        auto count = model->columnCount();
+        auto count = model->rowCount();
         Q_ASSERT(count > 0);
         auto delegate = new DelegatesDoubleSpinBox(tableView);
         delegate->OnEditorAboutToBeShown.Connect(CONNECTION_DEBUG_LOCATION, [](class QDoubleSpinBox* editor, const QModelIndex& index){
-            auto* unit = index.model()->headerData(index.column(), Qt::Vertical, UnitRole).value<const Measurement*>();
+            auto* unit = index.model()->headerData(index.row(), Qt::Vertical, UnitRole).value<const Measurement*>();
+            if(unit == nullptr){
+                return;
+            }
             editor->setSingleStep(unit->CurrentStep);
             editor->setDecimals(unit->CurrentPrecision);
             auto min = index.data(MinLimitRole);
