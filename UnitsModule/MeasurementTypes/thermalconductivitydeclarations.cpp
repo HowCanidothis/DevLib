dd7fda0c4aa1fa61be549f90ed509251c4e6408d
smag <smagilov@mail.ru>
2024-07-03 14:00:39 +0700

    Symbol

--- UnitsModule/MeasurementTypes/thermalconductivitydeclarations.cpp
+++ UnitsModule/MeasurementTypes/thermalconductivitydeclarations.cpp
@@ -4,7 +4,7 @@
 
 namespace ThermalConductivityUnits
 {
-    IMPLEMENT_MEASUREMENT_UNIT(WattMeterCelsius,             TR(MeasurementTr::tr("watt/meter/Celsius")),               TR(MeasurementTr::tr("W/(m*С)")),           1.0)
+    IMPLEMENT_MEASUREMENT_UNIT(WattMeterCelsius,             TR(MeasurementTr::tr("watt/meter/Celsius")),               TR(MeasurementTr::tr("W/(m*C)")),           1.0)
     IMPLEMENT_MEASUREMENT_UNIT(FootHourSquareFootFahrenheit, TR(MeasurementTr::tr("foot/hour/square foot/Fahrenheit")), TR(MeasurementTr::tr("BTU/(Hr*ft2*F)")),    1.7295772)
 }
 
