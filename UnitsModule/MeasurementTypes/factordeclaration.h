bec737622feeadc5af84c208ec8861571d62caff
smag <smagilov@mail.ru>
2025-02-11 19:33:08 +0700

    mesuremnt , vertical header

---
+++ UnitsModule/MeasurementTypes/factordeclaration.h
@@ -0,0 +1,18 @@
+#ifndef FACTORDECLARATION_H
+#define FACTORDECLARATION_H
+
+#include "UnitsModule/measurementdeclarations.h"
+
+namespace FrictionFactorUnits
+{
+    DECLARE_MEASUREMENT_UNIT(FrictionFactor)
+}
+DECLARE_MEASUREMENT(FrictionFactor)
+
+namespace SeparationFactorUnits
+{
+    DECLARE_MEASUREMENT_UNIT(SeparationFactor)
+}
+DECLARE_MEASUREMENT(SeparationFactor)
+
+#endif // FACTORDECLARATION_H
