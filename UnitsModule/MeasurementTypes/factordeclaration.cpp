216399bb73b66cb63c6fbb013efac4c4e27310dc
smag <smagilov@mail.ru>
2025-08-21 11:32:31 +0700

    wear factor

--- UnitsModule/MeasurementTypes/factordeclaration.cpp
+++ UnitsModule/MeasurementTypes/factordeclaration.cpp
@@ -20,3 +20,9 @@ namespace RateUnits
     IMPLEMENT_MEASUREMENT_UNIT(Rate, TR(MeasurementTr::tr("")), TR(MeasurementTr::tr("")), 1.0)
 }
 IMPLEMENT_MEASUREMENT(Rate, TR(MeasurementTr::tr("Rate")))
+
+namespace WearFactorUnits {
+    IMPLEMENT_MEASUREMENT_UNIT(Pascal, TR(MeasurementTr::tr("1 / Pa")), TR(MeasurementTr::tr("Pa-1")), 1.0)
+    IMPLEMENT_MEASUREMENT_UNIT(WearFactor, TR(MeasurementTr::tr("E-10 / psi")), TR(MeasurementTr::tr("E-10 / psi")), 1.e-10 / 6894.757)
+}
+IMPLEMENT_MEASUREMENT(WearFactor, TR(MeasurementTr::tr("Wear Factor")))
