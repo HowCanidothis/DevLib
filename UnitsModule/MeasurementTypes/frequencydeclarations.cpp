deaf827a803890417bdf6c6d0cd7b700cec9ba38
smag <smagilov@mail.ru>
2025-09-19 13:45:10 +0700

    units

--- UnitsModule/MeasurementTypes/frequencydeclarations.cpp
+++ UnitsModule/MeasurementTypes/frequencydeclarations.cpp
@@ -6,7 +6,10 @@ namespace FrequencyUnits
 {
     IMPLEMENT_MEASUREMENT_UNIT(RevolutionPerMinute, TR(MeasurementTr::tr("rpm")), TR(MeasurementTr::tr("rpm")), 1./60)
     IMPLEMENT_MEASUREMENT_UNIT(RevolutionPerSecond, TR(MeasurementTr::tr("rps")), TR(MeasurementTr::tr("1/sec")), 1.0)
-//    static const MeasurementUnit RevolutionPerSecond("RevolutionPerSecond", TR(MeasurementTr::tr("rps")), TR(MeasurementTr::tr("1/sec")), 1.0);
+
+    IMPLEMENT_MEASUREMENT_UNIT(StrokePerSecond, TR(MeasurementTr::tr("sps")), TR(MeasurementTr::tr("1/sec")), 1.0);
+    IMPLEMENT_MEASUREMENT_UNIT(StrokePerMinute, TR(MeasurementTr::tr("spm")), TR(MeasurementTr::tr("spm")), 1.0 / 60);
 }
 
 IMPLEMENT_MEASUREMENT(Frequency, TR(MeasurementTr::tr("Frequency")))
+IMPLEMENT_MEASUREMENT(StrokeFrequency, TR(MeasurementTr::tr("Stroke Frequency")))
